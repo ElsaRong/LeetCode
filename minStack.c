1ced42aae441bb44cc0930fb463a24a3d2c146fc
Elsa Rong <ElsaRong@163.com>
2021-01-28 00:09:30 +0800

    Update minStack.c
    [ADD]新增最小栈实现（提交通过，执行用时98.66%，内存消耗5.17%）

--- minStack.c
+++ minStack.c
@@ -1,31 +1,90 @@
-typedef struct {
+t#define MAXSIZE 1024*1024
 
+typedef struct {
+    int top;              //栈顶
+    int *initStack;       //原始栈
+    int *auxiliaryStack;  //辅助栈
+    int min;              //最小值
 } MinStack;
 
 /** initialize your data structure here. */
 
 MinStack* minStackCreate() {
+    static MinStack *minStack = NULL;
+    minStack = malloc(sizeof(MinStack));
+    if (NULL == minStack)
+    {
+        return NULL;
+    }
 
+    minStack->top = -1;
+    minStack->initStack = (int *)malloc(sizeof(int)*MAXSIZE);
+    minStack->auxiliaryStack = (int *)malloc(sizeof(int)*MAXSIZE);
+    if (NULL == minStack->initStack || NULL == minStack->auxiliaryStack)
+    {
+        return NULL;
+    }
+
+    memset(minStack->initStack, 0, sizeof(sizeof(int)*MAXSIZE));
+    memset(minStack->auxiliaryStack, 0, sizeof(sizeof(int)*MAXSIZE));
+
+    return minStack;
 }
 
 void minStackPush(MinStack* obj, int x) {
+    //原始栈先Push
+    obj->top++;
+    obj->initStack[obj->top] = x;
 
+    //辅助栈再Push：栈为空，直接入栈；栈不为空，比较栈顶
+    if (obj->top == 0)
+    {
+        obj->auxiliaryStack[obj->top] = x;
+    }
+    else 
+    {
+        if (obj->auxiliaryStack[obj->top - 1] < x)
+        {
+            obj->auxiliaryStack[obj->top] = obj->auxiliaryStack[obj->top - 1];
+        }
+        else
+        {
+            obj->auxiliaryStack[obj->top] = x;
+        }
+    }
+    
 }
 
 void minStackPop(MinStack* obj) {
-
+    obj->top--;
 }
 
 int minStackTop(MinStack* obj) {
-
+    return obj->initStack[obj->top];
 }
 
 int minStackGetMin(MinStack* obj) {
-
+    return obj->auxiliaryStack[obj->top];
 }
 
 void minStackFree(MinStack* obj) {
+    if (NULL != obj->initStack)
+    {
+        free(obj->initStack);
+        obj->initStack = NULL;
+    }
 
+    if (NULL != obj->auxiliaryStack)
+    {
+        free(obj->auxiliaryStack);
+        obj->auxiliaryStack = NULL;
+    }
+
+    if (NULL != obj)
+    {
+        free(obj);
+        obj = NULL;
+    }
 }
 
 /**
