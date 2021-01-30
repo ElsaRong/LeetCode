cf4cdf5ac2e8b29922798efe2676dcfeb5d55c64
Elsa Rong <ElsaRong@163.com>
2021-01-30 18:27:30 +0800

    Update CQueue.c
    [CHG]修改增删接口逻辑分类：两栈全空，或HeadStack为空，或TailStack为空三种情况（通过5/55个测试用例）

--- CQueue.c
+++ CQueue.c
@@ -24,10 +24,10 @@ void cQueueAppendTail(CQueue* obj, int value) {
     if (obj->qHead == -1 && obj->qTail == -1)
     {
         obj->QHeadStack[++obj->qHead] = value;
-    }
-    //上一次执行了队列头部元素删除，先重建再插入
-    if (obj->qHead == -1 && obj->qTail >= 0)
+    } 
+    else if (obj->qHead == -1 && obj->qTail >= 0)
     {
+        //上一次执行了队列头部元素删除，先重建再插入
         while (obj->qTail >= 0)
         {
             obj->QHeadStack[++i] = obj->QTailStack[obj->qTail--];
@@ -47,15 +47,20 @@ int cQueueDeleteHead(CQueue* obj) {
     {
         return -1;
     }
-    //上一次执行了队列尾部添加，HeadStack除栈底元素，逐个迁移到TailStack
-    if (obj->qHead >= 0 && obj->qTail == -1)
+    else if (obj->qHead >= 0 && obj->qTail == -1)
     {
+        //上一次执行了队列尾部添加，HeadStack除栈底元素，逐个迁移到TailStack
         while(obj->qHead > 0)
         {
-            obj->QTailStack[++i] = obj->QHeadStack[obj->qHead--];
+            obj->QTailStack[++obj->qTail] = obj->QHeadStack[obj->qHead--];
         }
         obj->qHead--;
-        return obj->QTailStack[0];
+        return obj->QHeadStack[0];
+    }
+    else if (obj->qHead == -1 && obj->qTail >= 0)
+    {
+        i = obj->qTail--;
+        return obj->QTailStack[i];
     }
     else
     {
