404d5384924f589527360001cd6bab05edd1a02e
Elsa Rong <ElsaRong@163.com>
2021-02-12 16:02:10 +0800

    Update kthLargest.c
    （1）调整堆排序，先构造小顶堆，通过堆顶和堆尾元素交换，生成降序序列；（2）降序数组初始化为10^4；（3）添加元素时分类讨论：序列未满k时直接插入，序列满k时比较最小值后再替换，通过9/10个用例

--- kthLargest.c
+++ kthLargest.c
@@ -11,7 +11,7 @@ void swap(int *x, int *y)
     *y = *x;
     *x = tmp;
 }
-void minHeap(int *arr, int p, int arrSize)
+void maxHeap(int *arr, int p, int arrSize)
 {
     int l, r = 0;
     //从左儿子开始，依次向下遍历
@@ -19,12 +19,12 @@ void minHeap(int *arr, int p, int arrSize)
     {
         if (l >= arrSize) break;
         r = l + 1;
-        //找出左右儿子最小值
+        //找出左右儿子最大值
         if (r < arrSize && arr[r] < arr[l])
         {
             l = r;
         }
-        //将左右儿子最小值和父节点交换
+        //将左右儿子最大值和父节点交换
         if (arr[l] < arr[p])
         {
             swap(&arr[l], &arr[p]);
@@ -42,7 +42,7 @@ void printfArr(int *arr, int arrSize)
     int i = 0;
     for (i = 0; i < arrSize; i++)
     {
-        printf("%4d", arr[i]);
+        printf("%3d", arr[i]);
     }
     printf("\n");
 }
@@ -50,30 +50,33 @@ void printfArr(int *arr, int arrSize)
 void heapSort(int *arr, int arrSize)
 {
     int i = 0;
+    if (arrSize <= 1) return;
     //从第一个非叶子开始，比较父子大小，构造小顶堆
     for (i = arrSize/2 - 1; i >=0; i--)
     {
-        minHeap(arr, i, arrSize);
+        maxHeap(arr, i, arrSize);
     }
-    //从最后一个叶子开始，和首元素交换后，构造小顶堆
+    //从最后一个叶子开始，和首元素交换后，生成降序序列
     for (i = arrSize - 1; i > 0; i--)
     {
         swap(&arr[0], &arr[i]);
-        minHeap(arr, 0, i);
+        maxHeap(arr, 0, i); //第三个入参，表示待调整的剩余heap大小
     }
 }
 
 KthLargest* kthLargestCreate(int k, int* nums, int numsSize) {
     int i = 0;
-    KthLargest *kthLargest = NULL;
+    static KthLargest *kthLargest = NULL;
     kthLargest = malloc(sizeof(KthLargest));
-    kthLargest->arr = malloc(sizeof(int) * numsSize);
+    kthLargest->arr = malloc(sizeof(int) * 10^4); 
+    memset(kthLargest->arr, 0, sizeof(int) * 10^4);
     for (i = 0; i < numsSize; i++)
     {
         kthLargest->arr[i] = nums[i];
     }
     //输入数据流经过堆排序，生成降序序列
     heapSort(kthLargest->arr, numsSize);
+    printf("初始数组:");
     printfArr(kthLargest->arr, numsSize);
     kthLargest->arrSize = numsSize;
     kthLargest->k = k;
@@ -83,8 +86,27 @@ KthLargest* kthLargestCreate(int k, int* nums, int numsSize) {
 /* 
  *  obj->arr是降序序列，新增一个元素时，先替换队尾元素再重新堆排序
  */
-int kthLargestAdd(KthLargest* obj, int val) {
-    obj->arr[obj->arrSize - 1] = val;
+int kthLargestAdd(KthLargest* obj, int val) 
+{
+    //如果降序序列未满k，元素加入序列尾，排序
+    if (obj->arrSize < obj->k)
+    {
+        obj->arr[obj->arrSize] = val; //降序序列大小为0
+        obj->arrSize++;
+    } 
+    else
+    {
+        //降序序列满k，如果小于序列尾，直接return；否则替换队列尾
+        if (obj->arr[obj->arrSize - 1] >= val)
+        {
+            printfArr(obj->arr, obj->arrSize);
+            return obj->arr[obj->k - 1];
+        }
+        else
+        {
+            obj->arr[obj->arrSize - 1] = val;
+        }
+    }
     heapSort(obj->arr, obj->arrSize);
     printfArr(obj->arr, obj->arrSize);
     return obj->arr[obj->k - 1];
