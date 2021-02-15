58ff7333511e7b104448674bd5172dc3709f1fc1
Elsa Rong <ElsaRong@163.com>
2021-02-15 21:45:44 +0800

    【顺时针打印矩阵】二维数组遍历，边界判断、顶点分类、健壮性判断、固定行/列遍历列/行

--- spiralOrder.c
+++ spiralOrder.c
@@ -19,20 +19,21 @@ int* spiralOrder(int** matrix, int matrixSize, int* matrixColSize, int* returnSi
     int *order = (int *)malloc(sizeof(int)*total);
     *returnSize = 0;
 
+    //特殊情况，矩阵[1]行为1，列为1
     while (left <= right && top <= bottom) {
-        for (column = left; column <= right; column++) {
+        for (column = left; column <= right; column++) {  //固定行，left->right
             order[(*returnSize)++] = matrix[top][column];
         }
-        for (row = top + 1; row <= bottom; row++) {
+        for (row = top + 1; row <= bottom; row++) {       //固定列，top->bottom
             order[(*returnSize)++] = matrix[row][right];
         }
 
         //上面2步骤可遍历一行或一列，如果大于等于2行且大于等于2列，再执行3、4步骤
         if (left < right && top < bottom) {
-            for (column = right-1; column > left; column--) {
+            for (column = right-1; column > left; column--) {      //固定行，right->left
                 order[(*returnSize)++] = matrix[bottom][column];
             }
-            for (row = bottom; row > top; row--) {
+            for (row = bottom; row > top; row--) {                 //固定列，bottom->top
                 order[(*returnSize)++] = matrix[row][left];
             }
         }
