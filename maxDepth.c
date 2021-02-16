6c9fcdcfa5d9a34c6874a54214a1b1f077693b6e
Elsa Rong <ElsaRong@163.com>
2021-02-16 18:50:49 +0800

    【二叉树的最大深度】深度优先搜索，递归实现

---
+++ maxDepth.c
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+int maxDepth(struct TreeNode* root){
+    if (NULL == root)
+        return 0 ;
+    
+    return 1 + fmax(maxDepth(root->left), maxDepth(root->right));
+}
