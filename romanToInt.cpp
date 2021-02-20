d6c28c2a80fd8d10be501b79afc6fffde26db452
Elsa Rong <ElsaRong@163.com>
2021-02-20 15:38:05 +0800

    【罗马数字转整数】C++

---
+++ romanToInt.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        std::map<char, int> romanTable;
+        romanTable.insert(make_pair('I', 1));
+        romanTable.insert(make_pair('V', 5));
+        romanTable.insert(make_pair('X', 10));
+        romanTable.insert(make_pair('L', 50));
+        romanTable.insert(make_pair('C', 100));
+        romanTable.insert(make_pair('D', 500));
+        romanTable.insert(make_pair('M', 1000));
+
+        int i = 0, sum = 0;
+        int diff = 0;
+        int sLen = s.size();
+        for(i = sLen - 1; i > 0; i--)
+        {
+            if(isSpecial(s[i-1], s[i]))
+            {
+                diff = romanTable.find(s[i])->second - romanTable.find(s[i-1])->second;
+                sum += diff;
+                i--;     //注意：前移2步，这里减1，因为条件语句中已经减过1
+            }
+            else
+            {
+                sum += romanTable.find(s[i])->second;
+            }
+        }
+        if (0 == i)
+            sum += romanTable.find(s[0])->second;
+        return sum;
+    }
+
+    bool isSpecial(char pre, char curr) 
+    {
+        if(curr == 'V' || curr == 'X')
+        {
+            if(pre == 'I')
+                return true;
+        }
+
+        if(curr == 'L' || curr == 'C')
+        {
+            if(pre == 'X')
+                return true;
+        }
+
+        if(curr == 'D' || curr == 'M')
+        {
+            if(pre == 'C')
+                return true;
+        }
+        return false;
+    }
+};
